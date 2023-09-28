a9d99290b368de290475bc96e90b49d19232bde3
alejo0450 <alejo0450@gmail.com>
2023-09-28 17:14:38 -0500

    project request and build docker container

--- web_request/main.py
+++ web_request/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app= FastAPI()#se crea una instancia  de esta ubicación
+
+@app.get('/')#Se crea un decorador con una ruta desde la que pueden ingresar desde la web.
+
+def get_list():
+    return [1,2,3]
+
+
+@app.get('/contact', response_class= HTMLResponse)#otro decorador para acceder a otra ruta
+
+def get_list():
+    return '''
+        <h1>Titulo</h1>'''
+
 
 def run():
     store.get_c()
